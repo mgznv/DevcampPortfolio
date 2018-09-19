c698b3acb8e9dd2cc2985fcc6ccb29433f60247b
Manuel Guzman <leunam.toshi@gmail.com>
2018-09-19 17:49:29 -0500

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    MgznvViewTool::Renderer.copyright 'Manuel Guzman', 'All rights reserved'
+  end
 end
