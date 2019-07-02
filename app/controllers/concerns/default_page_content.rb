4e874ffdb30ffeaac5e49681421145d1d5126eff
Manuel <leunam.toshi@gmail.com>
2019-07-02 10:35:37 -0500

    Se cambia el titulo a las paginas

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -6,8 +6,8 @@ module DefaultPageContent
   end
 
   def set_page_defaults
-    @page_title = "Devcamp Portfolio | My Portfolio Website"
-    @seo_keywords = 'Jordan Hudgens Portfolio'
+    @page_title = "mgznv | Mi portafolios profesional"
+    @seo_keywords = 'Manuel Guzman Portfolio'
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def login_helper style = ''
     if current_user.is_a?(GuestUser)
-      (link_to "Register", new_user_registration_path, class: style) +
+      (link_to "Registrar", new_user_registration_path, class: style) +
       " ".html_safe +
-      (link_to "Login", new_user_session_path, class: style)
+      (link_to "Iniciar Sesion", new_user_session_path, class: style)
     else
-      link_to "Logout", destroy_user_session_path, method: :delete, class: style
+      link_to "Salir", destroy_user_session_path, method: :delete, class: style
     end
   end
 
@@ -40,7 +40,7 @@ module ApplicationHelper
         },
         {
             url: portfolios_path,
-            title: 'Portafolio de Proyectos'
+            title: 'Portafolio'
         },
     ]
   end
